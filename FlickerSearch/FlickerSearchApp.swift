7d01abf9dcd2c7ba086bd019e629374ae53ced0e
Kristhian De Oliveira <kristhian.deoliveira@gmail.com>
2025-01-15 07:09:07 -0500

    Initial Commit

---
+++ FlickerSearch/FlickerSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerSearchApp.swift
+//  FlickerSearch
+//
+//  Created by Kristhian De Oliveira on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FlickerSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
